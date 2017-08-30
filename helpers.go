655e7aea7fd5de677bfb842679fe53e43a6d4db6
Frank Kung <kfanjian@gmail.com>
2017-08-31 07:08:58 +0800

    Add helper bytesToUint64.

--- helpers.go
+++ helpers.go
@@ -12,6 +12,10 @@ func uint64ToBytes(i uint64) []byte {
 	return buf
 }
 
+func bytesToUint64(bs []byte) uint64 {
+	return binary.BigEndian.Uint64(bs)
+}
+
 func createBuckets(name []byte) error {
 	return db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(name)
